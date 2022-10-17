6662bbc72bbeba02217ea494a31ea163cc1357c7
TanzimAhmedUtsho <utshotanzim@gmail.com>
2022-10-17 22:51:20 +0600

    another blog html file connect

--- js/script.js
+++ js/script.js
@@ -104,6 +104,7 @@ const loadNews = (category) => {
 
   /* adding inner html to new div */
   div.innerHTML = `
+  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
   <div class="card h-100 shadow" >
   <div class="row g-0">
     <div class="col-md-4 d-flex align-items-center">
@@ -146,6 +147,24 @@ const loadNews = (category) => {
   </div>
   
   
+</div>
+</button>
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Understood</button>
+      </div>
+    </div>
+  </div>
 </div>
   `;
 
